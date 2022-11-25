9160d7d91ecdf36898c30d4d61b0a910b1ddd77c
abhiraj2912 <abhirajaravind408@gmail.com>
2022-11-25 12:20:42 +0530

    todo

---
+++ src/app/addto/addto.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-addto',
+  templateUrl: './addto.component.html',
+  styleUrls: ['./addto.component.css']
+})
+export class AddtoComponent {
+
+}
